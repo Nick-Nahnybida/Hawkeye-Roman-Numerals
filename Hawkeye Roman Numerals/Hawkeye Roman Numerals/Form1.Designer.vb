8b6da4b2c20f1d5798955a6c0ef97e0756b7ebd3
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:07:45 -0500

    Add files via upload

---
+++ Hawkeye Roman Numerals/Hawkeye Roman Numerals/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(126, 123)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(25, 123)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 10
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(177, 9)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(125, 20)
+        Me.txtInput.TabIndex = 9
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(177, 67)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(125, 23)
+        Me.lblResult.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 26)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Equivalent Roman " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numeral:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 26)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Enter a decimal integer " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the range 1-10:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(235, 123)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 171)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Hawkeye Roman Numerals/Hawkeye Roman Numerals/Form1.vb
@@ -0,0 +1,171 @@
+﻿Public Class Form1
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim decInt As Integer
+
+        ' Convert variables
+        decInt = CInt(txtInput.Text)
+
+        ' Convert and display result
+        If decInt = 1 Then
+            lblResult.Text = "I"
+
+        ElseIf decInt = 2 Then
+            lblResult.Text = "II"
+
+        ElseIf decInt = 3 Then
+            lblResult.Text = "III"
+
+        ElseIf decInt = 4 Then
+            lblResult.Text = "IIII"
+
+        ElseIf decInt = 5 Then
+            lblResult.Text = "V"
+
+        ElseIf decInt = 6 Then
+            lblResult.Text = "VI"
+
+        ElseIf decInt = 7 Then
+            lblResult.Text = "VII"
+
+        ElseIf decInt = 8 Then
+            lblResult.Text = "VIII"
+
+        ElseIf decInt = 9 Then
+            lblResult.Text = "VIIII"
+
+        ElseIf decInt = 10 Then
+            lblResult.Text = "X"
+
+        ElseIf decInt = 11 Then
+            lblResult.Text = "XI"
+
+        ElseIf decInt = 12 Then
+            lblResult.Text = "XII"
+
+        ElseIf decInt = 13 Then
+            lblResult.Text = "XIII"
+
+        ElseIf decInt = 14 Then
+            lblResult.Text = "XIIII"
+
+        ElseIf decInt = 15 Then
+            lblResult.Text = "XV"
+
+        ElseIf decInt = 16 Then
+            lblResult.Text = "XVI"
+
+        ElseIf decInt = 17 Then
+            lblResult.Text = "XVII"
+
+        ElseIf decInt = 18 Then
+            lblResult.Text = "XVIII"
+
+        ElseIf decInt = 19 Then
+            lblResult.Text = "XVIIII"
+
+        ElseIf decInt = 20 Then
+            lblResult.Text = "XX"
+
+        ElseIf decInt = 21 Then
+            lblResult.Text = "XXI"
+
+        ElseIf decInt = 22 Then
+            lblResult.Text = "XXII"
+
+        ElseIf decInt = 23 Then
+            lblResult.Text = "XXIII"
+
+        ElseIf decInt = 24 Then
+            lblResult.Text = "XXIIII"
+
+        ElseIf decInt = 25 Then
+            lblResult.Text = "XXV"
+
+        ElseIf decInt = 26 Then
+            lblResult.Text = "XXVI"
+
+        ElseIf decInt = 27 Then
+            lblResult.Text = "XXVII"
+
+        ElseIf decInt = 28 Then
+            lblResult.Text = "XXVIII"
+
+        ElseIf decInt = 29 Then
+            lblResult.Text = "XXVIIII"
+
+        ElseIf decInt = 30 Then
+            lblResult.Text = "XXX"
+
+        ElseIf decInt = 31 Then
+            lblResult.Text = "XXXI"
+
+        ElseIf decInt = 32 Then
+            lblResult.Text = "XXXII"
+
+        ElseIf decInt = 33 Then
+            lblResult.Text = "XXXIII"
+
+        ElseIf decInt = 34 Then
+            lblResult.Text = "XXXIIII"
+
+        ElseIf decInt = 35 Then
+            lblResult.Text = "XXXV"
+
+        ElseIf decInt = 36 Then
+            lblResult.Text = "XXXVI"
+
+        ElseIf decInt = 37 Then
+            lblResult.Text = "XXXVII"
+
+        ElseIf decInt = 38 Then
+            lblResult.Text = "XXXVIII"
+
+        ElseIf decInt = 39 Then
+            lblResult.Text = "XXXVIIII"
+
+        ElseIf decInt = 40 Then
+            lblResult.Text = "XXXX"
+
+        ElseIf decInt = 41 Then
+            lblResult.Text = "XXXXI"
+
+        ElseIf decInt = 42 Then
+            lblResult.Text = "XXXXII"
+
+        ElseIf decInt = 43 Then
+            lblResult.Text = "XXXXIII"
+
+        ElseIf decInt = 44 Then
+            lblResult.Text = "XXXXIIII"
+
+        ElseIf decInt = 45 Then
+            lblResult.Text = "XXXXV"
+
+        ElseIf decInt = 46 Then
+            lblResult.Text = "XXXXVI"
+
+        ElseIf decInt = 47 Then
+            lblResult.Text = "XXXXVII"
+
+        ElseIf decInt = 48 Then
+            lblResult.Text = "XXXXVIII"
+
+        ElseIf decInt = 49 Then
+            lblResult.Text = "XXXXVIIII"
+
+        ElseIf decInt = 50 Then
+            lblResult.Text = "L"
+
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtInput.Clear()
+        lblResult.Text = ("")
+    End Sub
+End Class
